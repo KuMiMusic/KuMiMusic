c5fd8ce645bb160e5a8015dfc0653184adf7d6dd
xianfeng <xianfeng12006@163.com>
2014-08-10 15:00:42 +0800

    first commit

---
+++ KuMiMusic/XFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XFAppDelegate.h
+//  KuMiMusic
+//
+//  Created by qisheng on 14-8-10.
+//  Copyright (c) 2014年 KuMi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KuMiMusic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KuMiMusic
+//
+//  Created by qisheng on 14-8-10.
+//  Copyright (c) 2014年 KuMi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XFAppDelegate class]));
+    }
+}
